c8fb8137c5860a9f0db8efd5d9aac0a45fad100b
MarkD1916 <vmakd@gmail.com>
2023-02-19 17:15:14 +0300

    верстка, убрал лишний импорт

--- feature_blocker/src/main/java/com/example/feature_blocker/ui/RestrictionBottomSheet.kt
+++ feature_blocker/src/main/java/com/example/feature_blocker/ui/RestrictionBottomSheet.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.feature_blocker.R
 import com.example.feature_blocker.RestrictionFeatureContract
-import com.example.navigation.NavigationManager
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class RestrictionBottomSheet : RestrictionFeatureContract() {
 
@@ -20,6 +20,12 @@ class RestrictionBottomSheet : RestrictionFeatureContract() {
     override fun onCancel(dialog: DialogInterface) {
         super.onCancel(dialog)
         onDismissAction?.invoke() ?: throw Exception("You must put action if feature unavailable")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val behavior = BottomSheetBehavior.from(requireView().parent as View)
+        behavior.state = BottomSheetBehavior.STATE_EXPANDED
     }
 
 }
